1967767f9e64e51e3fe4f39e1d10c51673b1bc10
Hard1stf <hardikvijeta2@gmail.com>
2025-10-23 01:57:47 +0530

    code: apply duplication logic to work animation logic, and make it endless animation.

--- assets/js/script.js
+++ assets/js/script.js
@@ -94,7 +94,17 @@ servicesButton.forEach(button => {
 })
 
 /*=============== TESTIMONIALS OF DUPLICATE CARDS ===============*/
+// Duplicate image to make the animation work
 
+const tracks = document.querySelectorAll('.testimonial-content');
+tracks.forEach(track => {
+  const cards = [...track.children] // spread to make a static copy
+
+  // Duplicate cards only once
+  for(const card of cards){
+    track.appendChild(card.cloneNode(true));
+  };
+});
 
 /*=============== COPY EMAIL IN CONTACT ===============*/
 
